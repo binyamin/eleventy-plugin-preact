6260c1cec5b5982d731b29d05796a26ed7c82fd8
Binyamin Aron Green <binyamingreen@protonmail.com>
2022-03-30 13:09:42 -0400

    wrote a long & technical comment for a bit of code

--- .eleventy.js
+++ .eleventy.js
@@ -16,6 +16,23 @@ module.exports = function EleventyPreact(eleventyConfig, configGlobalOptions = {
         read: false,
         compile(inputContent, inputPath) {
             return async function(data) {
+                /*
+                  We can't pass `collections` or `eleventyComputed`
+                  as props.
+                  
+                  TL;DR - With the way we render the JSX, we
+                  get an error for those objects.
+                  
+                  Why?
+                  The way we render the JSX, we spawn a node.js
+                  script, and send the props to that process
+                  in real-time, via a cool feature called an
+                  "IPC channel".
+                  We serialize the props before sending them,
+                  using the native v8 module. It refuses to
+                  serialize `collections` and `eleventyComputed`.
+                  This may be because of circular references.
+                */
                 const {
                     collections,
                     eleventyComputed,
